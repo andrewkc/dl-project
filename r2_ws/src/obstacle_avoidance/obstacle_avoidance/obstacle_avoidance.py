09d4bb4baaf5e70403305473183ca58349b59f5b
andrewkc <andresjkcc@gmail.com>
2024-12-04 16:36:17 -0500

    add

--- r2_ws/src/obstacle_avoidance/obstacle_avoidance/obstacle_avoidance.py
+++ r2_ws/src/obstacle_avoidance/obstacle_avoidance/obstacle_avoidance.py
@@ -37,11 +37,7 @@ class Turtlebot3Obstacle(Node):
 
         if msg.ranges[0] < 0.425:
             # Retroceder
-            #self.move.linear.x = -2.0   
-            self.move.linear.x = -0.5   
-            #self.move.linear.x = -0.22 
-            #self.move.linear.x = -0.10
-            #self.move.linear.x = -0.05
+            self.move.linear.x = -0.05
             self.move.angular.z = 0.0
             self.pub.publish(self.move)
             self.get_clock().sleep_for(rclpy.time.Duration(seconds=1))
@@ -49,7 +45,6 @@ class Turtlebot3Obstacle(Node):
             # Decidir dirección de giro
             if msg.ranges[right_index] <= msg.ranges[left_index]:
                 self.move.linear.x = 0.0
-                #self.move.linear.x = 0.02
                 self.move.angular.z = -0.5
                 self.pub.publish(self.move)
                 self.get_clock().sleep_for(rclpy.time.Duration(seconds=1.0))
@@ -58,7 +53,6 @@ class Turtlebot3Obstacle(Node):
                 self.get_logger().info(" -> right")
             else:
                 self.move.linear.x = 0.0
-                #self.move.linear.x = 0.02
                 self.move.angular.z = 0.5
                 self.pub.publish(self.move)
                 self.get_clock().sleep_for(rclpy.time.Duration(seconds=1.0))
@@ -67,11 +61,7 @@ class Turtlebot3Obstacle(Node):
                 self.get_logger().info(" -> left")
 
             # Avanzar para evitar el obstáculo
-            #self.move.linear.x = 2.0
-            self.move.linear.x = 0.5
-            #self.move.linear.x = 0.22
-            #self.move.linear.x = 0.10
-            #self.move.linear.x = 0.05
+            self.move.linear.x = 0.05
             self.move.angular.z = 0.0
             self.pub.publish(self.move)
 
@@ -79,14 +69,9 @@ class Turtlebot3Obstacle(Node):
                 self.get_clock().sleep_for(rclpy.time.Duration(seconds=0.1))
                 self.pub.publish(self.move)
         else:
-            #self.move.linear.x = 2.0
-            self.move.linear.x = 0.5
-            #self.move.linear.x = 0.22
-            #self.move.linear.x = 0.10
-            #self.move.linear.x = 0.05
+            self.move.linear.x = 0.05
             self.move.angular.z = 0.0
             self.pub.publish(self.move)
-
 
 def main(args=None):
     rclpy.init(args=args)
